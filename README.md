b0d7595a6b5874ef2d48538141c27e4e51ab2c01
bifbi <54706029+bifbi@users.noreply.github.com>
2023-10-28 19:39:04 +0200

    1 úkol
    1. úkol hotov

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# engeto_data_academy_project
- Project for Engeto Academy
+#Project for Engeto Academy
+- - -
+## Struktura projektu
+- Soubory s odpovědi ve MarkDown formátu *.md, jež obsahují:
+	- Průvodní listinu s popisem kroků
+	- Informace ve výstupních datech 
+- Soubory s tabulkami ve SQL formátu *.sql
+
+## Zadání projektu
+Připravit datové podklady pro analytické oddělení týkající se dostupnosti základních potravin veřejnosti a to s ohledem na průměrné příjmy občanů v určitém časovém období. Kromě toho také vytvořit tabulku s údaji o HDP, GINI koeficientu a populaci dalších evropských států ve stejném období jako primární přehled pro Českou republiku.
+
+##Výzkumné otázky
+1. Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
+2. Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
+3. Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
+4. Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
+5. Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce, projeví se to na cenách potravin či mzdách ve stejném nebo násdujícím roce výraznějším růstem?
+
+## Odpovědi na výzkumné otázky
+1. Bohužel mzdy nerostou v průběhu let ve většině odvětvích, a to s výjimkou:
+		- Doprava a skladování
+		- Administrativní a podpůrné činnosti
+		- Zdravotní a sociální péče
+		- Ostatních činností
---
+++ ukol_1.sql
@@ -0,0 +1,75 @@
+-- Úkol 1
+-- výběr dat [mzdy, fyzický, ne NULL]
+SELECT *
+FROM czechia_payroll
+WHERE value_type_code = 5958 AND calculation_code = 100 AND industry_branch_code IS NOT NULL
+ORDER BY industry_branch_code, payroll_year, payroll_quarter;
+
+-- výpočet průměru pro jednotlivé odvětví v jednotlivých letech
+SELECT value, industry_branch_code, payroll_year, AVG(value) AS year_average
+FROM czechia_payroll
+WHERE value_type_code = 5958 AND calculation_code = 100 AND industry_branch_code IS NOT NULL
+GROUP BY payroll_year, industry_branch_code
+ORDER BY industry_branch_code, payroll_year, payroll_quarter;
+
+-- výpočet, zda je současný rok větší než předchozí rok
+WITH year_average AS (SELECT value, industry_branch_code, payroll_year, AVG(value) AS year_average
+FROM czechia_payroll
+WHERE value_type_code = 5958 AND calculation_code = 100 AND industry_branch_code IS NOT NULL
+GROUP BY payroll_year, industry_branch_code
+ORDER BY industry_branch_code, payroll_year, payroll_quarter)
+SELECT industry_branch_code, payroll_year, year_average,
+CASE
+	WHEN year_average < LAG(year_average, 1) OVER (PARTITION BY industry_branch_code ORDER BY payroll_year) THEN "DECREASE"
+	ELSE "INCREASE"
+END AS comparison
+FROM year_average;
+
+-- filtrace dat
+WITH year_comparisons AS (WITH year_average AS (SELECT value, industry_branch_code, payroll_year, AVG(value) AS year_average
+FROM czechia_payroll
+WHERE value_type_code = 5958 AND calculation_code = 100 AND industry_branch_code IS NOT NULL
+GROUP BY payroll_year, industry_branch_code
+ORDER BY industry_branch_code, payroll_year, payroll_quarter)
+SELECT industry_branch_code, payroll_year, year_average,
+CASE
+	WHEN year_average < LAG(year_average, 1) OVER (PARTITION BY industry_branch_code ORDER BY payroll_year) THEN "DECREASE"
+	ELSE "INCREASE"
+END AS comparison
+FROM year_average)
+SELECT DISTINCT(industry_branch_code), comparison
+FROM year_comparisons
+WHERE comparison = 'DECREASE';
+
+-- final code
+WITH decreases AS (WITH year_comparisons AS (WITH year_average AS (SELECT value, industry_branch_code, payroll_year, AVG(value) AS year_average
+FROM czechia_payroll
+WHERE value_type_code = 5958 AND calculation_code = 100 AND industry_branch_code IS NOT NULL
+GROUP BY payroll_year, industry_branch_code
+ORDER BY industry_branch_code, payroll_year, payroll_quarter)
+SELECT industry_branch_code, payroll_year, year_average,
+CASE
+	WHEN year_average < LAG(year_average, 1) OVER (PARTITION BY industry_branch_code ORDER BY payroll_year) THEN "DECREASE"
+	ELSE "INCREASE"
+END AS comparison
+FROM year_average)
+SELECT DISTINCT(industry_branch_code), comparison
+FROM year_comparisons
+WHERE comparison = 'DECREASE')
+SELECT code, name, comparison
+FROM czechia_payroll_industry_branch
+LEFT JOIN decreases ON decreases.industry_branch_code = czechia_payroll_industry_branch.code;
+
+-- final optimised code
+WITH decreases AS (SELECT value, industry_branch_code, payroll_year, AVG(value) AS year_average,
+CASE
+	WHEN AVG(value) < LAG(AVG(value), 1) OVER (PARTITION BY industry_branch_code ORDER BY payroll_year) THEN "DECREASE"
+	ELSE "INCREASE"
+END AS comparison
+FROM czechia_payroll
+WHERE value_type_code = 5958 AND calculation_code = 100 AND industry_branch_code IS NOT NULL
+GROUP BY payroll_year, industry_branch_code
+ORDER BY industry_branch_code, payroll_year, payroll_quarter)
+SELECT DISTINCT(code), name, comparison
+FROM czechia_payroll_industry_branch
+LEFT JOIN decreases ON decreases.industry_branch_code = czechia_payroll_industry_branch.code AND decreases.comparison = 'DECREASE';
